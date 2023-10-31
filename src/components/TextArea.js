f18d9d7c1020bf3e060ea89477cdb98f48d93349
aizentel <aizentel>
2023-10-31 19:19:37 +0600

    Colorful update

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -3,6 +3,9 @@ import {useEffect, useState} from "react";
 function TextArea() {
     const [inputText, setInputText] = useState('')
     const [key, setKey] = useState("Pres key")
+    const [bgColor, setBgColor] = useState('white')
+    const [colorChangingEnabled, setColorChangingEnabled] = useState(false)
+
     let mainText = "type something"
 
     const onKey = (event) => {
@@ -20,11 +23,45 @@ function TextArea() {
         }
     }, []);
 
+    function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+    const handleChange = (e) => {
+        setInputText(e.target.value)
+
+        if (colorChangingEnabled && e.target.value.length % 2 === 1) {
+            setBgColor(getRandomColor)
+        } else if (colorChangingEnabled && e.target.value.length % 2 === 0) {
+            setBgColor(getRandomColor)
+        } else {
+            setBgColor('white')
+        }
+    }
+
+    const handleCheckboxChange = () => {
+        setColorChangingEnabled(!colorChangingEnabled);
+    };
+
     return (
         <>
             <h1>{makeUpper(mainText)}</h1>
             <h2>{key}</h2>
-            <textarea id="1" cols="30" rows="10" value={inputText} onChange={(e) => setInputText(e.target.value)}></textarea>
+            <textarea value={inputText} onChange={handleChange} style={{backgroundColor: bgColor}}
+            rows="10" cols="100"></textarea>
+            <label>
+                Enable Color Change
+                <input
+                    type="checkbox"
+                    checked={colorChangingEnabled}
+                    onChange={handleCheckboxChange}
+                />
+            </label>
         </>
     )
 }
