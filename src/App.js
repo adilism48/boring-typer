5b93f263d005584c1f33cc829c492fa13b1ae3b5
aizentel <aizentel>
2023-11-01 16:48:10 +0600

    final version

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import TextArea from "./components/TextArea";
+import {useEffect, useState} from "react";
 
 function App() {
+    const [key, setKey] = useState("Pres key")
+
+    const onKey = (event) => {
+        setKey(event.key)
+    }
+
+    useEffect(() => {
+        document.addEventListener("keydown", onKey)
+        return () => {
+            document.removeEventListener("keydown", onKey)
+        }
+    }, [])
+
     return (
         <div className="App">
             <TextArea/>
+            <h2>{key}</h2>
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -1,27 +1,17 @@
-import {useEffect, useState} from "react";
+import {useState} from "react";
 
 function TextArea() {
     const [inputText, setInputText] = useState('')
-    const [key, setKey] = useState("Pres key")
     const [bgColor, setBgColor] = useState('white')
     const [colorChangingEnabled, setColorChangingEnabled] = useState(false)
 
-    let mainText = "type something"
+    const [titleText, setTitleText] = useState('type something');
+    const [textAreaVisible, setTextAreaVisible] = useState(false);
 
-    const onKey = (event) => {
-        setKey(event.key)
-    }
 
     function makeUpper(text) {
         return text.replace(inputText, inputText.toUpperCase())
     }
-
-    useEffect(() => {
-        document.addEventListener("keydown", onKey)
-        return () => {
-            document.removeEventListener("keydown", onKey)
-        }
-    }, []);
 
     function getRandomColor() {
         const letters = '0123456789ABCDEF';
@@ -44,24 +34,44 @@ function TextArea() {
         }
     }
 
+    const handleTitleChange = (event) => {
+        const newTitleText = event.target.value;
+        setTitleText(newTitleText);
+    }
+
     const handleCheckboxChange = () => {
         setColorChangingEnabled(!colorChangingEnabled);
-    };
+    }
+
+    const toggleTextAreas = () => {
+        setTextAreaVisible(!textAreaVisible);
+    }
 
     return (
         <>
-            <h1>{makeUpper(mainText)}</h1>
-            <h2>{key}</h2>
+            <h1>{makeUpper(titleText)}</h1>
             <textarea value={inputText} onChange={handleChange} style={{backgroundColor: bgColor}}
-            rows="10" cols="100"></textarea>
-            <label>
-                Enable Color Change
-                <input
-                    type="checkbox"
-                    checked={colorChangingEnabled}
-                    onChange={handleCheckboxChange}
-                />
-            </label>
+                      rows="10" cols="100"></textarea>
+            <div>
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={colorChangingEnabled}
+                        onChange={handleCheckboxChange}
+                    />
+                    Make not boring
+                </label>
+            </div>
+
+            <button className="buttonColorful" onClick={toggleTextAreas}>
+                {textAreaVisible ? 'Hide' : 'Change text'}
+            </button>
+            <div>
+                {textAreaVisible && (<>
+                    <p>You can copy text from somewhere and paste it here:</p>
+                    <textarea rows="10" cols="100" value={titleText} onChange={handleTitleChange}/>
+                </>)}
+            </div>
         </>
     )
 }
